adb569ad42926e7e2ad50d6df61bdbb17bc597b5
Geetesh Mandaogade <57803929+Geetesh2912@users.noreply.github.com>
2022-06-02 18:58:25 +0530

    Create 02.06.2022-transpose-matrix.cpp

---
+++ 02.06.2022-transpose-matrix.cpp
@@ -0,0 +1,18 @@
+//author: Geetesh Mandaogade
+//question link: https://leetcode.com/problems/transpose-matrix/
+//submission link: https://leetcode.com/submissions/detail/712795792/
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>> ans ( matrix[0].size(), vector<int> (matrix.size()) );
+        for (int i=0; i<matrix.size(); i++)
+        {
+            for (int j=0; j<matrix[0].size(); j++)
+            {
+                ans[j][i]=matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
